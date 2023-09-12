884afee2fc022b1c217b0fe7109040effa6bc90a
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-12 20:26:27 +0530

    User-Header Component✅

---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,87 @@
+import {
+  Avatar,
+  Box,
+  Flex,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  Text,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
+const UserHeader = () => {
+  const toast = useToast();
+
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        description: "Profile Link Copied",
+        status: "success",
+        duration: "3000",
+      });
+    });
+  };
+
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Mark Zuck
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>markzuckbg</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"grey.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Avatar name="Mark Zucker" src="/zuck-avatar.png" size={"xl"} />
+        </Box>
+      </Flex>
+      <Text>Cofounder, Billionaire, Phiantrophist</Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>3.2k followers</Text>
+          <Box w={"1"} h={"1"} bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copy Link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default UserHeader;
