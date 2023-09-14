78c2c6c89ceca14b369c77685ac85d661e82aa73
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-14 21:55:23 +0530

    SignUp User✨

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { loginUser, signupUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("signed up successfully");
-});
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
 export default router;
