0bb66afe1846093e6944688db109bcf1bd855b3a
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-15 00:10:42 +0530

    Get individual Profiles⚡

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,4 +136,26 @@ const updateUser = async (req, res) => {
   } catch (error) {}
 };
 
-export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
+const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+
+    if (!user) return res.status(400).json({ message: "User not found" });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in getting user profile: ", err.message);
+  }
+};
+
+export {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followUnFollowUser,
+  updateUser,
+  getUserProfile,
+};
