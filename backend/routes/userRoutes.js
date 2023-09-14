525319ad55663345a94c918ae09738d799645467
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-14 23:46:24 +0530

    Follow Unfollow users+ Middleware setup✨

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
+  followUnFollowUser,
   loginUser,
   logoutUser,
   signupUser,
 } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
@@ -11,5 +13,6 @@ router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser);
 
 export default router;
