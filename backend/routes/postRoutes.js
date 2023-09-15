654b1f11960b9ab02673f9d23ca261d21ff5570c
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-15 22:56:08 +0530

    feat: Delete post route✨

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createPost, getPost } from "../controllers/postController.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+} from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/:id", getPost);
+router.delete("/:id", protectRoute, deletePost);
 router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 
 export default router;
