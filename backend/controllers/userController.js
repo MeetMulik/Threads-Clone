628b80d914f1dfbb7941adfd4855465bcd233906
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-09-17 12:25:29 +0530

    feat: Liking + Commenting ✅

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
 const signupUser = async (req, res) => {
   try {
@@ -157,17 +158,26 @@ const updateUser = async (req, res) => {
 };
 
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
-  try {
-    const user = await User.findOne({ username })
-      .select("-password")
-      .select("-updatedAt");
+  const { query } = req.params;
 
-    if (!user) return res.status(400).json({ message: "User not found" });
+  try {
+    let user;
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
-    console.log("Error in getting user profile: ", err.message);
+    console.log("Error in getUserProfile: ", err.message);
   }
 };
 
